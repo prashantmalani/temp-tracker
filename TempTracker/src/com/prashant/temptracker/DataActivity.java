42dda8acfd5ab44f09e2f84748069cacb9d1aaab
Prashant Malani <p.malani@gmail.com>
2014-08-21 00:34:59 -0700

    Add second activity
    Also pass the URL string from first activity to second activity.
    
    Signed-off-by: Prashant Malani <p.malani@gmail.com>

--- TempTracker/src/com/prashant/temptracker/DataActivity.java
+++ TempTracker/src/com/prashant/temptracker/DataActivity.java
@@ -5,35 +5,41 @@ import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
+import android.widget.TextView;
 import android.os.Build;
-import android.util.Log;
 
-public class MainActivity extends ActionBarActivity {
+public class DataActivity extends ActionBarActivity {
 
-	private static final String TAG = "MainActivity";
-
+	private static final String TAG = "DataActivity";
+	private String mUrlString;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_data);
 
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
+		Intent intent = getIntent();
+		mUrlString = intent.getStringExtra("Url");
+
+		// Need to do something with this URL now...
+
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.data, menu);
 		return true;
 	}
 
@@ -60,31 +66,11 @@ public class MainActivity extends ActionBarActivity {
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
+			View rootView = inflater.inflate(R.layout.fragment_data, container,
 					false);
+			
 			return rootView;
 		}
-	}
-
-	/*
-	 * Just take the contents of the text box and send it to the next activity.
-	 */
-	public boolean onButton1Click(View v) {
-
-		EditText inputText = (EditText) findViewById(R.id.editText1);
-		String urlString = inputText.getText().toString();
-
-		if(urlString.equals("")) {
-			Log.e(TAG, "Invalid / blank URL provided!");
-			return false;
-		}
-
-		// Start the new activity, and populate the intent with the URL string
-		Intent intent = new Intent(this, DataActivity.class);
-		intent.putExtra("Url", urlString);
-		startActivity(intent);
-
-		return true;
 	}
 
 }
