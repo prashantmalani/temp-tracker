fa92b18ebb28063303594517f787ec1b0d539ef7
Prashant Malani <p.malani@gmail.com>
2014-09-10 23:42:30 -0700

    Read the raw data

--- read_init.py
+++ read_init.py
@@ -4,10 +4,23 @@
 import RPi.GPIO as GPIO
 import time
 
+# Store the input data here
+data = []
+
 GPIO.setmode(GPIO.BCM)
 
 # Stuff goes here
-GPIO.setup(4, GPIO.OUT, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(4, GPIO.OUT)
+GPIO.output(4, GPIO.LOW)
+time.sleep(.018)
+GPIO.output(4, GPIO.HIGH)
+#time.sleep(.000005)
 
+# Now we move into input mode and wait for the device to respond
+# with following falling edge
+GPIO.setup(4, GPIO.IN)
+for i in range(0,300):
+	data.append(GPIO.input(4))
 
+print data
 GPIO.cleanup()
